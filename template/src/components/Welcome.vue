86aa167ee2c549fa8bf95888144ab73b2919c8b5
Franco Bouly <fbouly@gmail.com>
2016-10-07 13:06:47 +0200

    Remove links when no router is used

--- template/src/components/Welcome.vue
+++ template/src/components/Welcome.vue
@@ -3,9 +3,11 @@
     <p>
       Welcome to your Vue.js app!
     </p>
+    {{#if routing}}
     <p>
       <router-link to="/">Home</router-link> - <router-link to="/hello/world">Hello</router-link>
     </p>
+    {{/if}}
   </div>
 </template>
 
